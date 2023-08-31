6000d994d9af0e7f9dbe2cf3dd997eb10814d618
Mitsuyoshi Yuge <yuge.mt@gmail.com>
2023-09-01 07:40:32 +0900

    some modification made.

--- ProjGeom2D.hs
+++ ProjGeom2D.hs
@@ -5,7 +5,11 @@ type HomogeneousCoordinate = (Double,Double,Double)
 
 type LineCoefficient = (Double,Double,Double)
 
-data (Double,Double,Double) => Element a =>  = Plane | Line a | Point a | Nothing
+data Element = Plane | Line LineCoefficient | Point HomogeneousCoordinate | Nothing
+
+data Line = Line LineCoefficient
+
+data Point = Point HomogeneousCoordinate
 
 dimension :: Element a -> Integer
 dimension e = case e of
@@ -14,7 +18,7 @@ dimension e = case e of
   Point _ -> 0
   ProjGeom2D.Nothing -> -1
 
-meet :: Element a -> Element a -> Element a
+meet :: Element -> Element -> Element
 meet _ ProjGeom2D.Nothing = ProjGeom2D.Nothing
 meet Plane e = e
 meet (Line a) (Line b)
@@ -28,7 +32,7 @@ meet (Point a) (Point b)
   | otherwise = ProjGeom2D.Nothing
 meet e1 e2 = meet e2 e1
 
-join :: Element a -> Element a -> Element a
+join :: Element -> Element -> Element
 join e ProjGeom2D.Nothing = e
 join Plane _ = Plane
 join (Line a) (Line b)
@@ -45,9 +49,15 @@ join e1 e2 = join e2 e1
 precision :: Double
 precision = 0.0001
 
-included :: Element a -> Element a -> Bool
+included :: Element -> Element -> Bool
+included _ Plane = True
+included Plane _ = False
+included Nothing _ = True
+include _ Nothig = False
 included (Line a) (Line b) = ((Line a) ProjGeom2D.== (Line b))
+included (Line _) (Point _) = False
 included (Point (x,y,z)) (Line (a,b,c)) = abs(a*x + b*y + c*z) < precision*(sqrt(a^2+b^2+c^2)*sqrt(x^2+y^2+z^2))
+included (Point (x1,y1,z1)) (Point (x2,y2,z2)) = False --todo: implement
 
 (==) (Line (a1,b1,c1)) (Line (a2,b2,c2)) =
   abs(a1*b2-a1*b1) < precision*(abs(a1)*abs(b2)+abs(a2)*abs(b1))/2.0
@@ -55,10 +65,10 @@ included (Point (x,y,z)) (Line (a,b,c)) = abs(a*x + b*y + c*z) < precision*(sqrt
 
 -- a symmetricity between point and line.
 
-incident :: Element a -> Element a -> Element b
+incident :: Line -> Line -> Point
 incident (Line (a1,b1,c1)) (Line (a2,b2,c2)) = Point ((b1*c2-b2*c1),(c1*a2-c2*a1),(a1*b2-b1*a2))
 
-aLine :: Element a -> Element a -> Element b
+aLine :: Point -> Point -> Line
 aLine (Point (x1,y1,z1)) (Point (x2,y2,z2)) = Line ((y1*z2-y2*z1),(z1*x2-z2*x1),(x1*y2-x2*y1))
 
 
